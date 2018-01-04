3385500edc759f9a44abfe944a7ea9ec86149c5b
Hannah Pullen <hannah.pullen@physics.ox.ac.uk>
2018-01-04 17:25:47 +0000

    Only check once for no jobs

--- .custom_functions/batch_prompt.zsh
+++ .custom_functions/batch_prompt.zsh
@@ -1,16 +1,19 @@
 function batch_prompt_precmd() {
     local BATCHINFO=""
-    local N_TESTING=$(qstat -ans | grep hancock | grep testing | wc -l)
-    local N_SHORT=$(qstat -ans | grep hancock | grep shor | wc -l)
-    local N_NORMAL=$(qstat -ans | grep hancock | grep normal | wc -l)
-    if [[ ${N_TESTING} -ne 0 ]]; then
-        BATCHINFO="%{$fg[cyan]%} ${N_TESTING}"
+    local SURNAME="hancock"
+    if [[ $(qstat -ans | grep $SURNAME | wc -l) -ne 0 ]]; then
+        local N_TESTING=$(qstat -ans | grep $SURNAME | grep testing | wc -l)
+        local N_SHORT=$(qstat -ans | grep $SURNAME | grep shor | wc -l)
+        local N_NORMAL=$(qstat -ans | grep $SURNAME | grep normal | wc -l)
+        if [[ ${N_TESTING} -ne 0 ]]; then
+            BATCHINFO="%{$fg[cyan]%} ${N_TESTING}"
+        fi
+        if [[ ${N_SHORT} -ne 0 ]]; then
+            BATCHINFO="${BATCHINFO} %{$fg[green]%} ${N_SHORT}"
+        fi
+        if [[ ${N_NORMAL} -ne 0 ]]; then
+            BATCHINFO="${BATCHINFO} %{$fg[yellow]%} ${N_NORMAL}"
+        fi
+        echo $BATCHINFO
     fi
-    if [[ ${N_SHORT} -ne 0 ]]; then
-        BATCHINFO="${BATCHINFO} %{$fg[green]%} ${N_SHORT}"
-    fi
-    if [[ ${N_NORMAL} -ne 0 ]]; then
-        BATCHINFO="${BATCHINFO} %{$fg[yellow]%} ${N_NORMAL}"
-    fi
-    echo $BATCHINFO
 }
